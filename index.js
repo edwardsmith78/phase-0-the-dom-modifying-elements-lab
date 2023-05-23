a32096faf20c0b7050022fa628c12f01f7172b1c
Edward Smith <esmith78@gmail.com>
2023-05-23 13:59:25 -0400

    modified elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Spongebob is the champion";
+
+newHeader.setAttribute("id", "victory");
